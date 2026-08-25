3a007a366525a62183cf8897d8e2bbed6655f5b2
MarkVPCC <goldsteinm@vpcc.edu>
2026-08-25 15:40:06 -0400

    start

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+    double hours,rate,pay;
+    cout
+
+    return 0;
+    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
+}
