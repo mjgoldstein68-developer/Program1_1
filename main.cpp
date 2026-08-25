4266b307a55824249310622420ff410ca0001ebd
MarkVPCC <goldsteinm@vpcc.edu>
2026-08-25 16:07:34 -0400

    start

--- main.cpp
+++ main.cpp
@@ -5,7 +5,14 @@ using namespace std;
 
 int main() {
     double hours,rate,pay;
-    cout
+    cout<<"How many hours did your work:"<<endl;
+    cin >>hours;
+    cout <<"What do you make an hour?"<<endl;
+    cin >>rate;
+    pay= hours *rate;
+    cout <<"For "<<hours<< "worked at "<<rate<< " and hour your pay is" <<pay;
+
+
 
     return 0;
     // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
