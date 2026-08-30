dc370a2305c289a5efbe3cb6ff65f95325ad6181
Mark Goldstein <mjgoldstein68@gmail.com>
2026-08-30 10:43:58 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
-using namespace std;
+include <iostream>
+using namespace std
 
 
 
@@ -10,10 +10,9 @@ int main() {
     cout <<"What do you make an hour?"<<endl;
     cin >>rate;
     pay= hours * rate;
-    cout <<"For "<<hours<< " worked at "<<rate<< " and hour your pay is : $" <<pay;
+    cout <<"For "<<hours<<  worked at "<<rate<< " and hour your pay is : $" <<pay;
 
 
 
     return 0;
     // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
-}
